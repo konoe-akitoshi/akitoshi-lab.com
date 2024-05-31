004b9bf9f2be67bd80c6d7ffdd81b28331a6d2a9
Akitoshi Saeki <51017077+konoe-akitoshi@users.noreply.github.com>
2024-05-31 18:39:16 +0900

    chore: Remove react & react-dom

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import UnoCSS from '@unocss/astro';
-import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://akitoshi-lab.com",
-  integrations: [UnoCSS(), react(), sitemap()]
+  integrations: [UnoCSS(), sitemap()]
 });
